3fc9b53e6779f23ac88ebfa351550de26dad85e5
Sam Cohen <samc1213@gmail.com>
2017-04-15 13:11:14 -0500

    Moved handler into class

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from Constants import BIG_FONT_HEIGHT_FRACTION, BACKGROUND_COLOR
 from TextLabel import TextLabel
 from FileReader import FileReader
 import time
-from Handler3 import *
+from Handler3 import Handler
 import threading
 
 
@@ -11,7 +11,7 @@ def mainGuiLoop():
     root = tk.Tk()
     root.overrideredirect(True)
     root.overrideredirect(False)
-    #root.attributes('-fullscreen', True)
+    # root.attributes('-fullscreen', True)
     root.configure(background=BACKGROUND_COLOR)
 
     reader = FileReader(root, 'dbFile.txt', 'inputFile.txt', 'outputFile.txt')
@@ -22,10 +22,9 @@ def mainGuiLoop():
 
 if __name__ == '__main__':
     try:
-        running = True
+        handler = Handler()
+        handler.StartWorkerThreads()
         mainGuiLoop()
-        mainThread = threading.Thread(name='Main', target=main1)
-        mainThread.start()
     except Exception as e:
         print e
         port.close()
--- Handler3.py
+++ Handler3.py
@@ -5,148 +5,133 @@ import time
 import logging
 import sys
 import serial
-
 from GPIO_Class import GPIO_Class
-LED=GPIO_Class()
-Button=GPIO_Class()
-
 from Thread_Data_Object import Thread_Data
-TD=Thread_Data()
-
-from FPS_Class import FPS_Class
-fps=FPS_Class()
-
-port = serial.Serial(
-        "/dev/ttyAMA0",
-        baudrate=9600,
-        timeout=None)	# <12>port = None
-
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
-
-running = False
-
-def Fingerprint():
-
-  Sys=TD.get_Sys_State()
-  counter=0
-  while Sys!='shutdown' and running:
-      logging.debug('IDLE')
-      if Sys=='enroll':
-          logging.debug('enroll')
-          ID=input('Enter 7 digit ID Number: \n')
-          OW=input('Enable Overwrite: \n(1-enable, 0-disable)\n')
-          fps.FPS_Get_Template(ID,OW)
-          TD.set_Sys_State('idle')
-          logging.debug('Changing sys to idle')
-      if Sys=='identify':
-         logging.debug('IDENTIFY')
-         ID=input('Enter 7 digit ID Number: \n')
-         if fps.FPS_Identify(ID):
-             TD.set_Alert_State('green')
-             logging.debug('Changing alert to green')
-         TD.set_Sys_State('idle')
-         logging.debug('Changing sys to idle')
-      Sys=TD.get_Sys_State()
-      time.sleep(.5)
-      counter=counter+1
-      if counter==300:
-          sys.exit(0)
-  logging.debug('Fingerprint shutting Down')
-
-def Alert():
-  Alert=TD.get_Alert_State()
-  Sys=TD.get_Sys_State()
-  LED.InitLED()
-  counter=0
-  while Sys!='shutdown' and running:
-      logging.debug('updating LED')
-      Alert=TD.get_Alert_State()
-      Sys=TD.get_Sys_State()
-      LED.LED_ON(Alert)
-      time.sleep(.5)
-      counter=counter+1
-      if counter==300:
-          sys.exit(0)
-
-  LED.Cleanup()
-  logging.debug('Alert shutting Down')
-
-def Timer(max_seconds):
-  counter=0
-  while counter<=max_seconds and running:
-      #logging.debug(counter)
-      time.sleep(.1)
-      counter=counter+.1
-      TD.set_Sec_Count(counter)
-
-  logging.debug('Timer counted to %d seconds',max_seconds)
+from fps_Class import fps_Class
 
 
+class Handler(object):
+    def __init__(self):
+        self.LED = GPIO_Class()
+        self.Button = GPIO_Class()
+        self.TD = Thread_Data()
+        self.fps = fps_Class()
 
-def Csense():
-  Alert=TD.get_Alert_State()
-  Sys=TD.get_Sys_State()
-  Button.InitButton()
-  counter=0
-  while Sys!='shutdown' and running:
-      logging.debug('Checking_Button')
-      Alert=TD.get_Alert_State()
-      Sys=TD.get_Sys_State()
-      if Button.ReadButton():
-          Alert=TD.get_Alert_State()
-          if Alert=='blue_':
-            TD.set_Alert_State('red__')
-            logging.debug('Changing alert to red')
+        self.port = serial.Serial(
+                "/dev/ttyAMA0",
+                baudrate=9600,
+                timeout=None)	# <12>port = None
+
+        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+
+        self.running = False
+
+    def DoWorkerThread(self):
+        threads = []
+        A = threading.Thread(name='Alert', target=self.Alert)
+        C = threading.Thread(name='Csense', target=self.Csense)
+        F = threading.Thread(name='self.fps', target=self.Fingerprint)
+        T = threading.Thread(name='Timer', target=Timer, args=(300,))
+
+        threads.append(A)
+        threads.append(C)
+        threads.append(F)
+        threads.append(T)
+
+        A.start()
+        C.start()
+        F.start()
+        T.start()
+        while self.TD.get_Sec_Count()<100:
+            if self.TD.get_Sec_Count()>=70:
+                self.TD.set_Sys_State('shuself.TDown')
+            elif self.TD.get_Sec_Count()>=10 and self.TD.get_Alert_State() != 'green':
+                self.TD.set_Sys_State('identify')
+            time.sleep(1)
+
+    def StartWorkerThreads(self):
+        self.running = True
+        workerThreads = threading.Thread(name='DoWorkerThread', target=self.DoWorkerThread)
+        workerThreads.start()
+
+    def Fingerprint(self):
+        Sys=self.TD.get_Sys_State()
+        counter=0
+        while Sys!='shuself.TDown' and self.running:
+            logging.debug('IDLE')
+            if Sys=='enroll':
+                logging.debug('enroll')
+                ID=input('Enter 7 digit ID Number: \n')
+                OW=input('Enable Overwrite: \n(1-enable, 0-disable)\n')
+                self.fps.self.fps_Get_Template(ID,OW)
+                self.TD.set_Sys_State('idle')
+                logging.debug('Changing sys to idle')
+            if Sys=='identify':
+                logging.debug('IDENTIFY')
+                ID=input('Enter 7 digit ID Number: \n')
+            if self.fps.self.fps_Identify(ID):
+                self.TD.set_Alert_State('green')
+                logging.debug('Changing alert to green')
+                self.TD.set_Sys_State('idle')
+                logging.debug('Changing sys to idle')
+            Sys=self.TD.get_Sys_State()
             time.sleep(.5)
-      else:
-        if Alert=='red__':
-            time.sleep(2)
-            TD.set_Alert_State('blue_')
-            logging.debug('Changing alert to blue')
-      time.sleep(.5)
-      counter=counter+1
-      if counter==300:
-          sys.exit(0)
+            counter=counter+1
+            if counter==300:
+                sys.exit(0)
+        logging.debug('Fingerprint shutting Down')
 
-  Button.Cleanup()
-  logging.debug('Csense shutting Down')
+    def Alert(self):
+        Alert=self.TD.get_Alert_State()
+        Sys=self.TD.get_Sys_State()
+        self.LED.Initself.LED()
+        counter=0
+        while Sys!='shuself.TDown' and self.running:
+            logging.debug('updating self.LED')
+            Alert=self.TD.get_Alert_State()
+            Sys=self.TD.get_Sys_State()
+            self.LED.self.LED_ON(Alert)
+            time.sleep(.5)
+            counter=counter+1
+            if counter==300:
+                sys.exit(0)
 
+        self.LED.Cleanup()
+        logging.debug('Alert shutting Down')
 
+    def Timer(self, max_seconds):
+        counter=0
+        while counter<=max_seconds and self.running:
+            #logging.debug(counter)
+            time.sleep(.1)
+            counter=counter+.1
+            self.TD.set_Sec_Count(counter)
 
-def main1():
-threads=[]
-  A=threading.Thread(name='Alert',target=Alert)
-  C=threading.Thread(name='Csense',target=Csense)
-  F=threading.Thread(name='FPS',target=Fingerprint)
-  T=threading.Thread(name='Timer',target=Timer,args=(300,))
+            logging.debug('Timer counted to %d seconds',max_seconds)
 
-
-  threads.append(A)
-  threads.append(C)
-  threads.append(F)
-  threads.append(T)
-
-  A.start()
-  C.start()
-  F.start()
-  T.start()
-  while TD.get_Sec_Count()<100:
-
-        if TD.get_Sec_Count()>=70:
-            TD.set_Sys_State('shutdown')
-        elif TD.get_Sec_Count()>=10 and TD.get_Alert_State()!='green':
-            TD.set_Sys_State('identify')
-        time.sleep(1)
-
-
-
-if __name__ == "__main__":
-    try:
-        main1()
-    except Exception as e:
-        print e
-        port.close()
-    finally:
-        time.sleep(2)
-        port.close()
-        print("Handler_Shutting_Down")
+    def Csense(self):
+        Alert=self.TD.get_Alert_State()
+        Sys=self.TD.get_Sys_State()
+        self.Button.Initself.Button()
+        counter=0
+        while Sys!='shuself.TDown' and self.running:
+            logging.debug('Checking_self.Button')
+            Alert=self.TD.get_Alert_State()
+            Sys=self.TD.get_Sys_State()
+            if self.Button.Readself.Button():
+                Alert=self.TD.get_Alert_State()
+                if Alert=='blue_':
+                    self.TD.set_Alert_State('red__')
+                    logging.debug('Changing alert to red')
+                    time.sleep(.5)
+            else:
+                if Alert=='red__':
+                    time.sleep(2)
+                    self.TD.set_Alert_State('blue_')
+                    logging.debug('Changing alert to blue')
+            time.sleep(.5)
+            counter=counter+1
+            if counter==300:
+                sys.exit(0)
+        self.Button.Cleanup()
+        logging.debug('Csense shutting Down')
